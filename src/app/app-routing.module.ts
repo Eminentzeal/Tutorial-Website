74fc958bb5f327fff62d2f9232da4b04cb85bbce
Ezekiel <ogunniwaezekiel@gmail.com>
2020-12-11 12:12:33 +0100

    homecomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,58 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CourseContentComponent } from './course-content/course-content.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { InstructorProfileComponent } from './instructor-profile/instructor-profile.component';
+import { LoginInstructorComponent } from './login-instructor/login-instructor.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ParticularCourseComponent } from './particular-course/particular-course.component';
+import { RegisterInstructorComponent } from './register-instructor/register-instructor.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SuperAdminComponent } from './super-admin/super-admin.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '',
     component: HomeComponent},
 
+    { path: 'footer',
+    component: FooterComponent},
+
   { path: 'home',
     component: HomeComponent},
 
-  { path: 'course-content',
-    component: CourseContentComponent},
+  { path: 'instructor-profile',
+    component: InstructorProfileComponent},
+
+  { path: 'login-instructor',
+    component: LoginInstructorComponent},
+
+  { path: 'login-user',
+    component: LoginUserComponent},
+
+  { path: 'navbar',
+    component: NavbarComponent},
+
+  { path: 'particular-course',
+    component: ParticularCourseComponent},
+
+  { path: 'register-instructor',
+    component: RegisterInstructorComponent},
+
+  { path: 'register-user',
+    component: RegisterUserComponent},
+
+  { path: 'reset-password',
+    component: ResetPasswordComponent},
+
+  { path: 'super-admin',
+    component:SuperAdminComponent},
+
+  { path: 'user-profile',
+    component: UserProfileComponent},
 ];
 
 @NgModule({
